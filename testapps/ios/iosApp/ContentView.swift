4e254b43147b1f356735dda1d42bd756f1dc028d
UedaSoft IT Solutions <max.ueda@uedasoft-it.com>
2026-09-21 14:40:50 -0300

    - Project cleanup

--- testapps/ios/iosApp/ContentView.swift
+++ testapps/ios/iosApp/ContentView.swift
@@ -2,15 +2,15 @@ import SwiftUI
 import hardware
 
 struct ContentView: View {
-    private let service = MirabilisBlueService(mock: false)
+   // private let service = MirabilisBlueService(mock: false)
     var body: some View {
         VStack(spacing: 16) {
             Text("Mirabilis Blue KMP")
                 .font(.title2)
             Text("BLE-MIRABILIS-BLUE")
                 .font(.caption)
-            Button("Start Scan") { service.startScan(timeout: 30_000) }
-            Button("Stop Scan") { service.stopScan() }
+           // Button("Start Scan") { service.startScan(timeout: 30_000) }
+           // Button("Stop Scan") { service.stopScan() }
         }
         .padding()
     }
